901a1bafcf43104795ea82906c45f69da0342838
antoneta-qtmanolis <antoningmanolis@gmail.com>
2023-11-01 22:56:19 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,22 +12,25 @@
     <body>
         <h1>My Subjects</h1>
         <a href="forms/addform.php">Add Subject</a><br/><br/>
-        <table width='100%' border= 1>
+        <table width='100%' style="border= 1px solid black;">
             <tr bgcolor='#CCCCCC'>
-                <td>ID</td>
-                <td>Subject Code</td>
-                <td>Subject Name</td>
-                <td>Action</td>
+                <th style="border: 1px solid black;">ID</td>
+                <th style="border: 1px solid black;">Subject Code</th>
+                <th style="border: 1px solid black;">Subject Name</th>
+                <th style="border: 1px solid black;">Action</th>
             </tr>
             <?php 
-                while($res = mysqli_fetch_array($result)) {
+                while($res = mysqli_fetch_assoc($result)) {
                     echo "<tr>";
-                    echo "<td>" .$res['subject_id']."</td>";
-                    echo "<td>" .$res['subject_code']."</td>";
-                    echo "<td>" .$res['subject_name']."</td>";
-                    echo "<td><a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> | 
+                    echo "<td style='border: 1px solid black; text-align:center;'>" .$res['subject_id']. "</td>";
+                    echo "<td style='border: 1px solid black; text-align:center;'>" .$res['subject_code']."</td>";
+                    echo "<td style='border: 1px solid black; text-align:center;'>" .$res['subject_name']."</td>";
+                    echo "<td style='border: 1px solid black; text-align:center;'> 
+                            <a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> | 
                             <a href=\"functions/delete.php?id=$res[subject_id]\"
-                            onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+                            onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a>
+                        </td>";
+                    echo "</tr>";
                 }
             ?>
         </table>
