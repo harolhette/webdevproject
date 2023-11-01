a7e49104ab3e33e7a181faed653800f03c40cd99
antoneta-qtmanolis <antoningmanolis@gmail.com>
2023-11-01 23:09:59 +0000

    Update delete.php

--- functions/delete.php
+++ functions/delete.php
@@ -2,9 +2,9 @@
     // including the database connection file
     include_once("../dbConnection/mysqlconfig_connection.php");
     // getting id of the data from url
-    $id = $_GET['id'];
+    $id = $_GET['subject_id'];
     // deleting the row from table
-    mysqli_query($dbc, "DELETE FROM tblsubject WHERE subject_id='$id'");
+    mysqli_query($dbc, "DELETE FROM tblsubjects WHERE subject_id='$id'");
     // redirecting to the display page (index.php in our case)
     header("Location:../index.php")
 ?>
