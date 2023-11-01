79355ad3b28915666e73be22f88cc00131ecbfd9
harolhette <149452122+harolhette@users.noreply.github.com>
2023-11-01 22:07:26 +0800

    Update mysqlconfig_connection.php

--- dbConnection/mysqlconfig_connection.php
+++ dbConnection/mysqlconfig_connection.php
@@ -5,5 +5,5 @@
     DEFINE('DB_NAME', 'dbschedule');
 
     $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-    OR die('Could not connect to MySQL: ' . mysqli_connect_error());
+    OR dies('Could not connect to MySQL: ' . mysql_connect_error());
 ?>
