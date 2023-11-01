7d64d96536dd7475e9b0b40ff1f3dd735c5118c8
antoneta-qtmanolis <antoningmanolis@gmail.com>
2023-11-01 22:25:36 +0000

    Update fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,5 +1,5 @@
 <?php
     //fetching data
-    $query = "SELECT * FROM tblsubject";
-    $result = mysqli_query($dbc, $qurey); //using mysqli_query
+    $query = "SELECT * FROM tblsubjects";
+    $result = mysqli_query($dbc, $query); //using mysqli_query
 ?>
