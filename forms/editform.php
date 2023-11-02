9440d2268db2d380a0f2ec091bd142a7735f9bc8
antoneta-qtmanolis <antoningmanolis@gmail.com>
2023-11-02 00:08:10 +0000

    Update editform.php

--- forms/editform.php
+++ forms/editform.php
@@ -4,33 +4,35 @@
     // getting id of the data from url
     $id = $_GET['id'];
     // selecting data associated with this particular id
-    $result = mysql_query($dbc, "SELECT * FROM tblsubjects WHERE subject_id=$id");
-    while($res = mysqli_fetch_array($result)); {
+    $result = mysqli_query($dbc, "SELECT * FROM tblsubjects WHERE subject_id=$id");
+    while($res = mysqli_fetch_array($result)) {
         $code = $res['subject_code'];
         $name = $res['subject_name'];
     }
 ?>
 <!DOCTYPE html>
-<html>
-    <head>
+<html lang="em">
+<head>
+        <meta charset="UTF-8">
+        <meta name = "viewport" content="width-device-width, initial-scale=1.0">
         <title>Edit Data</title>
     </head>
     <body>
         <h1>Edit Subject</h1>
         <a href="../index.php">Home</a>
         <br></br>
-        <form action="form1" method="post" action="../functions/edit.php">
-            <table border="0">
+        <form action="../functions/edit.php" method="post" name="form1">
+            <table width="25%" style="border: 1px solid black;">
                 <tr>
                     <td>Subject Code</td>
-                    <td><input type="text" name="subject_code" value="<?php echo $code;?>"></td>
+                    <td><input type="text" name="code" value="<?php echo $code;?>"></td>
                 </tr>
                 <tr>
                     <td>Subject Name</td>
-                    <td><input type="text" name="subject_name" value="<?php echo $name;?>"></td>
+                    <td><input type="text" name="name" value="<?php echo $name;?>"></td>
                 </tr>
                 <tr>
-                    <td><input type="hidden" name="subject_id" value="<?php echo $_GET['id'];?>"></td>
+                    <td><input type="hidden" name="id" value="<?php echo $_GET['id'];?>"></td>
                     <td><input type="submit" name="update" value="Update"></td>
                 </tr>
             </table>
