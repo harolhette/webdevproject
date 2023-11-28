3ec2fd52eba76e403441a052bab334931a92a3be
harolhette <maryharolhetterom@gmail.com>
2023-11-28 16:27:59 +0800

    Update fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,5 +1,5 @@
 <?php
     //fetching data
-    $query = "SELECT * FROM tblsubjects";
+    $query = "SELECT * FROM tblsubjects LEFT JOIN tblsyllabus ON tblsyllabus.subject_id = tblsubjects.subject_id";
     $result = mysqli_query($dbc, $query); //using mysqli_query
 ?>
